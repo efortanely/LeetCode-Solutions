7b537f47d6e4360eb10cb89f88d2afb92c3bac98
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2019-10-26 17:26:53 -0500

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -6,12 +6,12 @@ public:
         for(int i = 0; i < nums.size(); i++){
             int n1 = nums[i];
             int n2 = target - n1;
-            num_set.insert(pair<int, int>(n1, i));
             auto j = num_set.find(n2);
             if(j != num_set.end()){
                 vector<int> ans{i, j->second};
                 return ans;
             }
+            num_set.insert(pair<int, int>(n1, i));
         }
         return null;
     }
