cbb0109822bdcdd7052264b7b53efa5827949868
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2019-10-26 16:15:13 -0500

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numSet = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int n1 = nums[i];
+            int n2 = target - n1;
+            numSet.put(n1, i);
+            if(numSet.containsKey(n2) == true){
+                int j = numSet.get(n2);
+                int[] ans = {i,j};
+                return ans;
+            }
+        }
+        return null;
+    }
+}
