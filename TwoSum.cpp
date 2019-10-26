4c7c9b5f5b1a498c79c98eeef6a2309ee827bc6f
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2019-10-26 16:33:25 -0500

    Create TwoSum.cpp

---
+++ TwoSum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> null;
+        map<int, int> num_set;
+        for(int i = 0; i < nums.size(); i++){
+            int n1 = nums[i];
+            int n2 = target - n1;
+            num_set.insert(pair<int, int>(n1, i));
+            auto j = num_set.find(n2);
+            if(j != num_set.end()){
+                vector<int> ans{i, j->second};
+                return ans;
+            }
+        }
+        return null;
+    }
+};
