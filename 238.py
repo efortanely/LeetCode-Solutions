7b001bc0a32b638cfd072834e1595d47e5293215
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2019-10-27 14:02:03 -0500

    Create 238.py

---
+++ 238.py
@@ -0,0 +1,18 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        M1 = [1 for x in range(len(nums) + 1)]
+        M2 = [1 for x in range(len(nums) + 1)]
+        nums2 = []
+        
+        for i in range(len(nums)):
+            M1[i] = M1[i-1] * nums[i]
+            
+        for i in range(len(nums) - 1, -1, -1):
+            M2[i] = M2[i+1] * nums[i]
+            
+        print(M1, M2)
+            
+        for i in range(len(nums)):
+            nums2.append(M1[i-1] * M2[i+1])
+        
+        return nums2
