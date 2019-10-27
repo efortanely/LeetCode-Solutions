1687993d100c7a5fe4b5eaa5913e632d84dd02a0
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2019-10-26 22:19:50 -0500

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,4 +1,5 @@
 class Solution:
+    '''
     def isMatch(self, s: str, p: str) -> bool:
         if not p:
             return not s
@@ -11,3 +12,21 @@ class Solution:
         #Case 3: Use the single c/. value
         else:
             return first_ch and self.isMatch(s[1:], p[1:])
+    '''
+    
+    def isMatch(self, s: str, p: str) -> bool:
+        M = [[False for c in range(len(p)+1)] for r in range(len(s)+1)]
+        M[-1][-1] = True
+        for i in range(len(s), -1, -1):
+            for j in range(len(p) - 1, -1, -1):
+                first_ch = i < len(s) and (p[j] == '.' or p[j] == s[i])
+    
+                #Case 1: Ignore the wildcard and skip the pattern
+                #Case 2: Keep the wildcard and continue using the pattern
+                if j+1 < len(p) and p[j + 1] is '*':
+                    M[i][j] = M[i][j+2] or (first_ch and M[i+1][j])
+                #Case 3: Use the single c/. value
+                else:
+                    M[i][j] = first_ch and M[i+1][j+1]
+        return M[0][0]
+            
