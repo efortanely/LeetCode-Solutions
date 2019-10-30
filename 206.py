6f8ae917ad25c153bfb8a551d23eee6faecf775b
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2019-10-29 19:34:29 -0500

    Create 206.py

---
+++ 206.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        tail = None
+        
+        while head:
+            temp = head
+            head = head.next
+            temp.next = tail
+            tail = temp
+        
+        return tail
