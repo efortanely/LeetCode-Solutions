b9313eb48d65b7290e6af421012c1d48c94f5804
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2019-10-31 11:43:29 -0500

    Update 79.py

--- 79.py
+++ 79.py
@@ -2,28 +2,23 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         for i in range(len(board)):
             for j in range(len(board[i])):
-                if board[i][j] is word[0]:
-                    if self.exist_square(i, j, board, word[1:]):
-                        return True
+                if self.exist_square(i, j, board, word):
+                    return True
         return False
-        
-        return exists
                 
     def exist_square(self, i: int, j: int, board: List[List[str]], word: str) -> bool:
         if not word:
             return True
         
+        if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]) or board[i][j] is not word[0]:
+            return False
+        
         temp = board[i][j]
         board[i][j] = ''
         
-        for new_i, new_j in {(i-1,j), (i,j-1), (i,j+1), (i+1,j)}:
-            nbr = ''
-            if new_i >= 0 and new_j >= 0 and new_i < len(board) and new_j < len(board[0]):
-                nbr = board[new_i][new_j]
-            if nbr is word[0]:
-                if self.exist_square(new_i, new_j, board, word[1:]):
-                    return True
+        exist = self.exist_square(i-1,j,board,word[1:]) or self.exist_square(i,j-1,board,word[1:]) or\
+                self.exist_square(i,j+1,board,word[1:]) or self.exist_square(i+1,j,board,word[1:])
         
         board[i][j] = temp
         
-        return False
+        return exist
