fec6ee35975bef4275af60038f80646c87537100
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2019-11-02 19:19:19 -0500

    BFS + LCA
    Thank you to 2 previous problems I've solved for the help!

--- 865.py
+++ 865.py
@@ -7,30 +7,39 @@
 
 class Solution:
     def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
-        if root.left is None and root.right is None:
-            return root
-        else:
-            tree = self.nodeAtDeepest(None, root, 0)[0]
-            if tree.left is None or tree.right is None:
-                if tree.left:
-                    return tree.left
-                else:
-                    return tree.right
-            else:
-                return tree
-        
+        lowestLevel = self.levelHelper(root, 0, [])[-1]
+        if len(lowestLevel) == 1:
+            return lowestLevel[0]
+        elif len(lowestLevel) == 2:
+            return self.lowestCommonAncestor(root, lowestLevel[0].val, lowestLevel[1].val)
+        elif len(lowestLevel) == 3:
+            left = self.lowestCommonAncestor(root, lowestLevel[0].val, lowestLevel[1].val)
+            right = self.lowestCommonAncestor(root, lowestLevel[1].val, lowestLevel[2].val)
+            return self.lowestCommonAncestor(root, left.val, right.val)
     
-    def nodeAtDeepest(self, parent: TreeNode, node: TreeNode, layer: int) -> (TreeNode, int):
-        if not node:
-            return (TreeNode(0), float('-inf'))
-        if node.left is None and node.right is None:
-            return (parent, layer)
-        else:
-            left = self.nodeAtDeepest(node, node.left, layer + 1) 
-            right = self.nodeAtDeepest(node, node.right, layer + 1)
-            left_layer = left[1]
-            right_layer = right[1]
-            if left_layer > right_layer:
-                return left
-            else:
-                return right
+    def lowestCommonAncestor(self, root: TreeNode, n1: int, n2: int) -> TreeNode: 
+        if not root: 
+            return None
+        if root.val == n1 or root.val == n2: 
+            return root  
+        
+        left = self.lowestCommonAncestor(root.left, n1, n2)  
+        right = self.lowestCommonAncestor(root.right, n1, n2) 
+        
+        if left and right: 
+            return root  
+        
+        return left if left else right
+    
+    def levelHelper(self, node: TreeNode, level: int, levels: List[List[int]]) -> List[List[int]]:
+            if not node:
+                return
+            
+            if level == len(levels):
+                levels.append([])
+            levels[level].append(node)
+            
+            self.levelHelper(node.left, level + 1, levels)
+            self.levelHelper(node.right, level + 1, levels)
+            
+            return levels
