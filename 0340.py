ec959eb6aa8abe0ce6be2e5b51da01a46023d5f4
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2020-05-24 18:38:46 -0500

    Added class to make things simpler

--- 0340.py
+++ 0340.py
@@ -1,23 +1,48 @@
+class CharacterSet:
+    def __init__(self):
+        self.seen = dict()
+    
+    def add(self, char: str):
+        self.seen[char] = (self.seen[char] if char in self.seen else 0) + 1
+        
+    def remove(self, char: str):
+        if char in self.seen:
+            self.seen[char] -= 1
+            if self.seen[char] == 0:
+                del self.seen[char]
+    
+    def k_char(self, k: int):
+        return len(self.seen.keys()) <= k
+    
+    def num_chars(self):
+        return sum(self.seen.values())
+    
+    def get_dict(self):
+        return self.seen
+
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
-        start, maxlen = 0, 0
-        dic = dict()
+        if not k or not s:
+            return 0
         
-        for end in range(len(s)):
-            c = s[end]
+        left, right = 0, 0
+        max_str = 0
+        c = CharacterSet()
+        
+        c.add(s[right])
+        
+        if c.k_char(k):
+            max_str = max(max_str, 1)
+        
+        while left <= right:
+            if c.k_char(k) and right < len(s) - 1:
+                right += 1
+                c.add(s[right])
+            else:
+                c.remove(s[left])
+                left += 1
             
-            #update dictionary counts
-            dic[c] = dic[c] + 1 if c in dic else 1
-
-            #if dictionary size is larger than k, 
-            #update start pointer and dictionary
-            while(len(dic) > k):
-                c = s[start]
-                dic[c] -= 1
-                if dic[c] == 0:
-                    del dic[c]
-                start += 1
-            
-            maxlen = max(maxlen, end - start + 1)
-            
-        return maxlen
+            if c.k_char(k):
+                max_str = max(max_str, c.num_chars())
+        
+        return max_str
