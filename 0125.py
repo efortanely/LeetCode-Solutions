bfe7ab648ae5dc0395ae47b041a57568c047280b
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2020-05-23 23:57:17 -0500

    made even simpler! I'm improving :)

--- 0125.py
+++ 0125.py
@@ -1,7 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        #replaces all occurences of characters that aren't a-z, 0-9 with ''
-        reg = re.compile(r'[^%s]' % (string.ascii_lowercase + string.digits))
-        s = reg.sub('', s.lower())
-        k = len(s) // 2
-        return s[0:k] == s[-1:len(s)-k-1:-1]
+        regex = re.compile("[\W]")
+        s = regex.sub('', s.lower())
+        return s == s[::-1]
