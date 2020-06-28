931161ccd99feb90f598d27236a2d6af3ed299cb
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2020-06-27 19:37:52 -0500

    Better solution

--- 0426.py
+++ 0426.py
@@ -1,26 +1,34 @@
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, val, left, right):
+    def __init__(self, val, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 """
+
 class Solution:
     def treeToDoublyList(self, root: 'Node') -> 'Node':
-        if root is None:
-            return root
+        prev, head = None, None
+        def inorderTraversal(node: 'Node') -> None:
+            nonlocal prev, head
+            if not node:
+                return
+
+            inorderTraversal(node.left)
+            
+            if not prev:
+                head = node
+            else:
+                prev.right = node
+                node.left = prev
+            
+            prev = node
+            
+            inorderTraversal(node.right)
         
-        ptrs = self.treeToDoubleListHelper([], root)
-        for i in range(len(ptrs)):
-            node = ptrs[i]
-            node.left = ptrs[i-1]
-            node.right = ptrs[(i+1) % len(ptrs)]
-        return ptrs[0]
-    
-    def treeToDoubleListHelper(self, pointers, root: 'Node'):
-        if root:
-            pointers = self.treeToDoubleListHelper(pointers, root.left)
-            pointers.append(root)
-            pointers = self.treeToDoubleListHelper(pointers, root.right)
-        return pointers
+        inorderTraversal(root)
+        if head:
+            head.left = prev
+            prev.right = head
+        return head
