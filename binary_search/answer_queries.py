de1e8947e6ae5195cf45ae612d927452d0341e9a
Elizabeth Fortanely <elizabeth.fortanely@gmail.com>
2024-07-13 18:33:31 -0500

    resolve answer queries

--- binary_search/answer_queries.py
+++ binary_search/answer_queries.py
@@ -2,7 +2,29 @@ from typing import List
 
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-        pass
+        nums.sort()
+        prefix = [nums[0]]
+        ans = []
+
+        for i in range(1, len(nums)):
+            prefix.append(prefix[-1] + nums[i])
+
+        n = len(prefix)
+
+        for query in queries:
+            left = 0
+            right = n
+
+            while left < right:
+                mid = (left + right) // 2
+                if prefix[mid] > query:
+                    right = mid
+                else:
+                    left = mid + 1
+
+            ans.append(left)
+
+        return ans
 
 def main():
     runner = Solution()
