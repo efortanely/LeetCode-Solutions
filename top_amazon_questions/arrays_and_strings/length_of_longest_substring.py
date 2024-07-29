e68ee871f6cc94ed47071cb42d6a0e5ff1078f6b
Elizabeth Fortanely <elizabeth.fortanely@gmail.com>
2024-07-29 08:16:33 -0500

    save

--- top_amazon_questions/arrays_and_strings/length_of_longest_substring.py
+++ top_amazon_questions/arrays_and_strings/length_of_longest_substring.py
@@ -1,9 +1,8 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         left = ans = 0
-        n = len(s)
 
-        for right in range(n):
+        for right in range(len(s)):
             if len(set(s[left:right+1])) != right - left + 1:
                 left += 1
             ans = max(ans, right - left + 1)
