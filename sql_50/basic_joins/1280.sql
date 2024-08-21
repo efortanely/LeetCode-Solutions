684c56de189f28a61a51a14c9beb1318b809ca8d
Elizabeth Fortanely <elizabeth.fortanely@gmail.com>
2024-08-21 15:25:24 -0500

    students and examinations

---
+++ sql_50/basic_joins/1280.sql
@@ -0,0 +1,23 @@
+SELECT
+    s.student_id,
+    s.student_name,
+    sub.subject_name,
+    IFNULL (grouped.attended_exams, 0) AS attended_exams
+FROM
+    Students AS s
+    CROSS JOIN Subjects AS sub
+    LEFT JOIN (
+        SELECT
+            student_id,
+            subject_name,
+            COUNT(*) AS attended_exams
+        FROM
+            Examinations
+        GROUP BY
+            student_id,
+            subject_name
+    ) AS grouped ON s.student_id = grouped.student_id
+    AND sub.subject_name = grouped.subject_name
+ORDER BY
+    s.student_id,
+    sub.subject_name
